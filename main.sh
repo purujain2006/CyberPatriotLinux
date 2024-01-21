5323264e85f4b7230e6ab6ea80f369cb8d649165
drpain365 <81000890+drpain365@users.noreply.github.com>
2024-01-21 02:16:46 -0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -10,81 +10,68 @@
 
 #Add space for clarity
 #--------------------------#
-
 function space(){
     echo " "
     echo " "
 }
-
 #--------------------------#
 
-#red "(message)" makes message red, green "(message)" makes message green, etc.
-
+# red "(message)" makes message red, green "(message)" makes message green, etc.
 #Red
 #--------------------------#
-
 function red(){
   printf "\033[31m$1\033[0m"
 }
-
 #--------------------------#
 
 #Green
 #--------------------------#
-
 function green(){
   printf "\033[32m$1\033[0m"
 }
-
 #--------------------------#
 
 #Yellow
 #--------------------------#
-
 function yellow(){
   printf "\033[33m$1\033[0m"
 }
-
 #--------------------------#
 
 #Blue
 #--------------------------#
-
 function blue(){
   printf "\033[34m$1\033[0m"
 }
-
 #--------------------------#
 
 #Purple
 #--------------------------#
-
 function purple(){
   printf "\033[35m$1\033[0m"
 }
-
 #--------------------------#
 
 #Dark Blue
 #--------------------------#
-
 function dblue(){
   printf "\033[36m$1\033[0m"
 }
-
 #--------------------------#
-
-###############################################################################################################################
 
 
 #prompts user for current user
-#------------------------------------------------------------------------------------------
+#--------------------------#
 space
 yellow "Current working directory is" ; pwd
 space
 blue "What is the name of the user you are currently using? (ex: haha if /home/haha/Desktop/..."
 read CUSER
-#------------------------------------------------------------------------------------------
+#--------------------------#
+
+
+###############################################################################################################################
+
 
 
 
@@ -107,7 +94,6 @@ sudo apt update
 
 #Secure Update Settings
 #--------------------------#
-
 #security repository
 echo """
 APT::Periodic::Update-Package-Lists "1";
@@ -124,13 +110,10 @@ APT::Periodic::AutocleanInterval "0";
 APT::Periodic::Unattended-Upgrade "1";
 """ > /etc/apt/apt.conf.d/20auto-upgrades
 
-#other notify
+#notify immediately
 sudo apt-get install dbus-x11
 clear
 su -l $CUSER -c 'gsettings set com.ubuntu.update-notifier regular-auto-launch-interval 0'
-
 #--------------------------#
-
-
 
 ###############################################################################################################################
