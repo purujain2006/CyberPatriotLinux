ecd7d7bcb7374705a14e6c49a825f771250df823
drpain365 <81000890+drpain365@users.noreply.github.com>
2024-01-21 18:01:21 -0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -162,4 +162,45 @@ for line in $(cat /etc/passwd | cut -d ":" -f 1)
 	done
 #-----------------------------------------------------------------------------------------
 
+#Look for HIDDEN USERS
+#-----------------------------------------------------------------------------------------
+clear
+# Define minimum UID for normal users
+MIN_UID=1000
+
+# Define system account names that should never be removed
+SYSTEM_ACCOUNTS=("root" "daemon" "bin" "sys" "sync" "games" "man" "lp" "mail" "news" "uucp" "proxy" "www-data" "backup" "list" "irc" "gnats" "nobody" "systemd-network" "systemd-resolve" "syslog" "messagebus" "_apt" "lxd" "uuidd" "dnsmasq" "landscape" "pollinate" "sshd")
+
+space
+echo -n "Scanning for "; red "hidden users..."
+
+
+# Find users with UID < MIN_UID
+users_to_remove=$(awk -F':' -v min_uid="$MIN_UID" '{ if ($3 < min_uid) print $1 }' /etc/passwd)
+
+for user in $users_to_remove; do
+    if [[ ! " ${SYSTEM_ACCOUNTS[@]} " =~ " ${user} " ]]; then
+    	space
+        echo "Found hidden user: $user"
+        space
+        # Prompt for confirmation
+        read -p "Do you want to remove user $user? [y/n] " -n 1 -r
+	space
+ 	echo
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+            # Remove user
+            sudo deluser $user
+            if [ $? -eq 0 ]; then
+                echo "Hidden User $user removed successfully."
+            else
+                echo "FAILED to remove user $user."
+            fi
+        else
+            echo "Aborted removal of user $user."
+        fi
+        sleep 0.5s
+        clear
+    fi
+done
+#-----------------------------------------------------------------------------------------
 
